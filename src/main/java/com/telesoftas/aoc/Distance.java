ce3b5836326d6ddee4dfbc86160de7e2242ad739
Nerijus Eimanavičius <nermnv@telesoftas.com>
2019-12-06 09:09:49 +0200

    Day 3 part 2

--- src/main/java/com/telesoftas/aoc/Distance.java
+++ src/main/java/com/telesoftas/aoc/Distance.java
@@ -1,8 +1,9 @@
 package com.telesoftas.aoc;
 
 import java.awt.*;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class Distance {
 
@@ -10,48 +11,48 @@ public class Distance {
         Distance distance = new Distance();
 
         String input = "R8,U5,L5,D3\n" +
-            "U7,R6,D4,L4";
+                       "U7,R6,D4,L4";
 
         String[] split = input.split("\n");
 
-        Set<Point> wire1 = distance.calculateCoords(split[0]);
-        Set<Point> wire2 = distance.calculateCoords(split[1]);
+        var wire1 = distance.calculateCoords(split[0]);
+        var wire2 = distance.calculateCoords(split[1]);
 
-        wire1.retainAll(wire2);
-
-        wire1.stream()
-            .map(point -> Math.abs(point.x) + Math.abs(point.y))
+        wire1.entrySet().stream()
+            .filter(entry -> wire2.containsKey(entry.getKey()))
+            .map(entry -> wire2.get(entry.getKey()) + entry.getValue())
             .min(Integer::compareTo)
             .ifPresent(System.out::println);
     }
 
-    public Set<Point> calculateCoords(String vectors) {
+    public Map<Point, Integer> calculateCoords(String vectors) {
         int x = 0;
         int y = 0;
+        AtomicInteger points = new AtomicInteger();
 
-        Set<Point> wire = new HashSet<>();
+        Map<Point, Integer> wire = new HashMap<>();
         // one wire here
         for (String vector : vectors.split(",")) {
             int distance = Integer.parseInt(vector.substring(1));
             switch (vector.charAt(0)) {
                 case 'R':
                     for (int i = 0; i < distance; i++) {
-                        wire.add(new Point(++x, y));
+                        wire.computeIfAbsent(new Point(++x, y), __ -> points.incrementAndGet());
                     }
                     break;
                 case 'U':
                     for (int i = 0; i < distance; i++) {
-                        wire.add(new Point(x, ++y));
+                        wire.computeIfAbsent(new Point(x, ++y), __ -> points.incrementAndGet());
                     }
                     break;
                 case 'L':
                     for (int i = 0; i < distance; i++) {
-                        wire.add(new Point(--x, y));
+                        wire.computeIfAbsent(new Point(--x, y), __ -> points.incrementAndGet());
                     }
                     break;
                 case 'D':
                     for (int i = 0; i < distance; i++) {
-                        wire.add(new Point(x, --y));
+                        wire.computeIfAbsent(new Point(x, --y), __ -> points.incrementAndGet());
                     }
                     break;
             }
