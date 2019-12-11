884ae969581cb8272a76e45d92bb3542b26ad334
Nerijus Eimanavičius <nermnv@telesoftas.com>
2019-12-11 16:25:40 +0200

    Day 5. Part 1. END

--- src/main/java/com/telesoftas/aoc/Intcode.java
+++ src/main/java/com/telesoftas/aoc/Intcode.java
@@ -8,27 +8,13 @@ public class Intcode {
 
     public static void main(String[] args) {
         Output output = new Output();
-        String input = "1,0,0,3,1,1,2,3,1,3,4,3,1,5,0,3,2,13,1,19,1,9,19,23,2,23,13,27,1,27,9,31,2,31,6,35,1,5,35,39,1,10,39,43,2,43,6,47,1,10,47,51,2,6,51,55,1,5,55,59,1,59,9,63,1,13,63,67,2,6,67,71,1,5,71,75,2,6,75,79,2,79,6,83,1,13,83,87,1,9,87,91,1,9,91,95,1,5,95,99,1,5,99,103,2,13,103,107,1,6,107,111,1,9,111,115,2,6,115,119,1,13,119,123,1,123,6,127,1,127,5,131,2,10,131,135,2,135,10,139,1,13,139,143,1,10,143,147,1,2,147,151,1,6,151,0,99,2,14,0,0";
+        String input = "3,225,1,225,6,6,1100,1,238,225,104,0,1001,152,55,224,1001,224,-68,224,4,224,1002,223,8,223,1001,224,4,224,1,224,223,223,1101,62,41,225,1101,83,71,225,102,59,147,224,101,-944,224,224,4,224,1002,223,8,223,101,3,224,224,1,224,223,223,2,40,139,224,1001,224,-3905,224,4,224,1002,223,8,223,101,7,224,224,1,223,224,223,1101,6,94,224,101,-100,224,224,4,224,1002,223,8,223,101,6,224,224,1,224,223,223,1102,75,30,225,1102,70,44,224,101,-3080,224,224,4,224,1002,223,8,223,1001,224,4,224,1,223,224,223,1101,55,20,225,1102,55,16,225,1102,13,94,225,1102,16,55,225,1102,13,13,225,1,109,143,224,101,-88,224,224,4,224,1002,223,8,223,1001,224,2,224,1,223,224,223,1002,136,57,224,101,-1140,224,224,4,224,1002,223,8,223,101,6,224,224,1,223,224,223,101,76,35,224,1001,224,-138,224,4,224,1002,223,8,223,101,5,224,224,1,223,224,223,4,223,99,0,0,0,677,0,0,0,0,0,0,0,0,0,0,0,1105,0,99999,1105,227,247,1105,1,99999,1005,227,99999,1005,0,256,1105,1,99999,1106,227,99999,1106,0,265,1105,1,99999,1006,0,99999,1006,227,274,1105,1,99999,1105,1,280,1105,1,99999,1,225,225,225,1101,294,0,0,105,1,0,1105,1,99999,1106,0,300,1105,1,99999,1,225,225,225,1101,314,0,0,106,0,0,1105,1,99999,1008,677,677,224,1002,223,2,223,1006,224,329,1001,223,1,223,8,677,226,224,102,2,223,223,1006,224,344,101,1,223,223,1107,226,226,224,1002,223,2,223,1006,224,359,1001,223,1,223,1108,677,226,224,102,2,223,223,1005,224,374,1001,223,1,223,1007,226,226,224,102,2,223,223,1006,224,389,1001,223,1,223,108,677,677,224,1002,223,2,223,1005,224,404,1001,223,1,223,1007,677,677,224,102,2,223,223,1005,224,419,1001,223,1,223,8,226,677,224,102,2,223,223,1005,224,434,101,1,223,223,1008,677,226,224,102,2,223,223,1006,224,449,1001,223,1,223,7,677,677,224,102,2,223,223,1006,224,464,1001,223,1,223,8,226,226,224,1002,223,2,223,1005,224,479,1001,223,1,223,7,226,677,224,102,2,223,223,1006,224,494,1001,223,1,223,7,677,226,224,1002,223,2,223,1005,224,509,101,1,223,223,107,677,677,224,102,2,223,223,1006,224,524,101,1,223,223,1007,677,226,224,102,2,223,223,1006,224,539,101,1,223,223,107,226,226,224,1002,223,2,223,1006,224,554,101,1,223,223,1008,226,226,224,102,2,223,223,1006,224,569,1001,223,1,223,1107,677,226,224,1002,223,2,223,1005,224,584,101,1,223,223,1107,226,677,224,102,2,223,223,1005,224,599,101,1,223,223,1108,226,677,224,102,2,223,223,1005,224,614,101,1,223,223,108,677,226,224,102,2,223,223,1005,224,629,101,1,223,223,107,226,677,224,102,2,223,223,1006,224,644,1001,223,1,223,1108,226,226,224,1002,223,2,223,1006,224,659,101,1,223,223,108,226,226,224,102,2,223,223,1005,224,674,101,1,223,223,4,223,99,226";
 
-        // [0-99]
-        int noun = 12;
-        int verb = 2;
+        Integer[] inputs = parseIntcodeInput(input);
 
-        for (int i = 0; i <= 99; i++) {
-            for (int j = 0; j <= 99; j++) {
+        Integer[] memory = intcode(inputs, 1, output);
 
-                Integer[] inputs = parseIntcodeInput(input);
-
-                inputs[1] = i;
-                inputs[2] = j;
-
-                Integer[] memory = intcode(inputs, output);
-                if (memory[0] == 19690720) {
-                    System.out.printf("%d %d \n", i, j); // 19690720
-                    return;
-                }
-            }
-        }
+        System.out.println(output.getValue());
     }
 
     public static Integer[] parseIntcodeInput(final String input) {
@@ -39,26 +25,88 @@ public class Intcode {
             .toArray(Integer[]::new);
     }
 
-    public static Integer[] intcode(final Integer[] mem, final Output output) {
+    public static Integer[] intcode(final Integer[] mem, final int input, final Output output) {
+        label:
         for (int i = 0; i < mem.length; ) {
 
-            var command = mem[i];
+            Opcode opcode = parseOpcode(mem[i]);
 
-            if (command.equals(1)) { // addition
-                mem[mem[i + 3]] = mem[mem[i + 1]] + mem[mem[i + 2]];
-                i += 4;
-            } else if (command.equals(2)) { // multiplication
-                mem[mem[i + 3]] = mem[mem[i + 1]] * mem[mem[i + 2]];
-                i += 4;
-            } else if (command.equals(4)) { // output
-                output.write(mem[mem[i + 1]]);
-                i += 2;
-            } else if (command.equals(99)) { // halt
-                break;
+            switch (opcode.getOperation()) {
+                case 1:  // addition
+                    mem[mem[i + 3]] = take(mem, mem[i + 1], opcode.getModeFirst())
+                                      + take(mem, mem[i + 2], opcode.getModeSecond());
+                    i += 4;
+                    break;
+                case 2:  // multiplication
+                    mem[mem[i + 3]] = take(mem, mem[i + 1], opcode.getModeFirst()) * take(
+                        mem,
+                        mem[i + 2],
+                        opcode.getModeSecond()
+                    );
+                    i += 4;
+                    break;
+                case 3:  // input
+                    mem[mem[i + 1]] = input;
+                    i += 2;
+                    break;
+                case 4:  // output
+                    output.write(take(mem, mem[i + 1], opcode.getModeFirst()));
+                    i += 2;
+                    break;
+                case 99:  // halt
+                    break label;
             }
         }
 
         return mem;
+    }
+
+    private static Integer take(final Integer[] mem, final Integer value, final int mode) {
+        if (mode == 0) {
+            return mem[value];
+        }
+        return value;
+    }
+
+    public static Opcode parseOpcode(final int opcodeWithModes) {
+        int operation = opcodeWithModes % 100;
+        int modeFirst = opcodeWithModes / 100 % 10;
+        int modeSecond = opcodeWithModes / 1000;
+        return new Opcode(operation, modeFirst, modeSecond, 0);
+    }
+
+    public static class Opcode {
+
+        private final int opration;
+
+        private final int modeFirst;
+
+        private final int modeSecond;
+
+        private final int modeThird;
+
+        public Opcode(final int opration, final int modeFirst, final int modeSecond, final int modeThird) {
+            this.opration = opration;
+            this.modeFirst = modeFirst;
+            this.modeSecond = modeSecond;
+            this.modeThird = modeThird;
+        }
+
+        public int getOperation() {
+            return opration;
+        }
+
+        public int getModeFirst() {
+            return modeFirst;
+        }
+
+        public int getModeSecond() {
+            return modeSecond;
+        }
+
+        public int getModeThird() {
+            return modeThird;
+        }
     }
 
     public static class Output {
--- src/test/java/com/telesoftas/aoc/IntcodeTest.java
+++ src/test/java/com/telesoftas/aoc/IntcodeTest.java
@@ -1,7 +1,10 @@
 package com.telesoftas.aoc;
 
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import com.telesoftas.aoc.Intcode.Opcode;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -16,11 +19,97 @@ class IntcodeTest {
 
     @Test
     void pase_addition_instruction() {
-        String input = "1,1,2,0,99";
+        String program = "1,1,2,0,99";
 
-        Integer[] memory = Intcode.intcode(Intcode.parseIntcodeInput(input), output);
+        Integer[] memory = Intcode.intcode(Intcode.parseIntcodeInput(program), 0, output);
 
         assertEquals(3, memory[0]);
+    }
+
+    @Test
+    void pase_input_instruction() {
+        // Opcode 3 takes a single integer as input and saves it to the position given by its only parameter. For example, the instruction 3,50 would take an input value and store it at address 50.
+        String program = "3,0,99";
+        int input = 1;
+
+        Integer[] memory = Intcode.intcode(Intcode.parseIntcodeInput(program), input, output);
+
+        assertArrayEquals(new Integer[]{input, 0, 99}, memory);
+    }
+
+    @Test
+    void parse_opcode_with_mode_2() {
+        /*
+ABCDE
+ 1002
+
+DE - two-digit opcode,      02 == opcode 2
+ C - mode of 1st parameter,  0 == position mode
+ B - mode of 2nd parameter,  1 == immediate mode
+ A - mode of 3rd parameter,  0 == position mode,
+                                  omitted due to being a leading zero
+         */
+        String program = "1002,2,3,0";
+
+        Integer[] memory = Intcode.intcode(Intcode.parseIntcodeInput(program), 0, output);
+
+        assertEquals(9, memory[0]);
+    }
+
+    @Test
+    void parse_opcode_with_mode_1() {
+
+        String program = "1001,2,3,0";
+
+        Integer[] memory = Intcode.intcode(Intcode.parseIntcodeInput(program), 0, output);
+
+        assertEquals(6, memory[0]);
+    }
+
+    @Test
+    void parse_opcode_with_mode_4() {
+
+        String program = "104,5";
+
+        Integer[] memory = Intcode.intcode(Intcode.parseIntcodeInput(program), 0, output);
+
+        assertEquals(5, output.getValue());
+    }
+
+    @Test
+    void name() {
+        Opcode opcode = Intcode.parseOpcode(1);
+
+        assertAll(
+            () -> assertEquals(1, opcode.getOperation()),
+            () -> assertEquals(0, opcode.getModeFirst()),
+            () -> assertEquals(0, opcode.getModeSecond()),
+            () -> assertEquals(0, opcode.getModeThird())
+        );
+    }
+
+    @Test
+    void name1() {
+        Opcode opcode = Intcode.parseOpcode(101);
+
+        assertAll(
+            () -> assertEquals(1, opcode.getOperation()),
+            () -> assertEquals(1, opcode.getModeFirst()),
+            () -> assertEquals(0, opcode.getModeSecond()),
+            () -> assertEquals(0, opcode.getModeThird())
+        );
+    }
+
+    @Test
+    void name2() {
+        Opcode opcode = Intcode.parseOpcode(1001);
+
+        assertAll(
+            () -> assertEquals(1, opcode.getOperation(), "op"),
+            () -> assertEquals(0, opcode.getModeFirst(), "first"),
+            () -> assertEquals(1, opcode.getModeSecond(), "second"),
+            () -> assertEquals(0, opcode.getModeThird(), "third")
+        );
     }
 
     @Test
@@ -28,7 +117,7 @@ class IntcodeTest {
         // Opcode 4 outputs the value of its only parameter. For example, the instruction 4,50 would output the value at address 50.
         Integer[] program = Intcode.parseIntcodeInput("4,2,99");
 
-        Integer[] memory = Intcode.intcode(program, output);
+        Integer[] memory = Intcode.intcode(program, 0, output);
 
         assertEquals(99, output.getValue());
     }
@@ -37,7 +126,7 @@ class IntcodeTest {
     void golden_master() {
         String input = "1,12,2,3,1,1,2,3,1,3,4,3,1,5,0,3,2,13,1,19,1,9,19,23,2,23,13,27,1,27,9,31,2,31,6,35,1,5,35,39,1,10,39,43,2,43,6,47,1,10,47,51,2,6,51,55,1,5,55,59,1,59,9,63,1,13,63,67,2,6,67,71,1,5,71,75,2,6,75,79,2,79,6,83,1,13,83,87,1,9,87,91,1,9,91,95,1,5,95,99,1,5,99,103,2,13,103,107,1,6,107,111,1,9,111,115,2,6,115,119,1,13,119,123,1,123,6,127,1,127,5,131,2,10,131,135,2,135,10,139,1,13,139,143,1,10,143,147,1,2,147,151,1,6,151,0,99,2,14,0,0";
 
-        Integer[] memory = Intcode.intcode(Intcode.parseIntcodeInput(input), output);
+        Integer[] memory = Intcode.intcode(Intcode.parseIntcodeInput(input), 0, output);
 
         assertEquals(3306701, memory[0]);
     }
