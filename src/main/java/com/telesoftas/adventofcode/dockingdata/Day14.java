33b93f3173ea10f0ed503692cc66ad69aa5954e6
Nerijus Eimanavičius <nerijus.eimanavicius@telesoftas.com>
2020-12-14 23:08:46 +0200

    feat: implement Day 14: Docking Data Part 2

--- src/main/java/com/telesoftas/adventofcode/dockingdata/Day14.java
+++ src/main/java/com/telesoftas/adventofcode/dockingdata/Day14.java
@@ -4,53 +4,88 @@ import lombok.extern.log4j.Log4j2;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
+import java.util.function.Consumer;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import java.util.stream.Stream;
 
 import static java.util.Objects.requireNonNull;
+import static java.util.regex.Pattern.compile;
 
 @Log4j2
 public class Day14 {
 
-    private static byte[] mask = new byte[]{};
+    public static final Pattern PATTERN = compile("(mask = (?<mask>[01X]+))|(mem\\[(?<index>\\d+)] = (?<value>\\d+))");
 
     public static void main(String[] args) throws IOException {
         try (InputStream input = ClassLoader.getSystemResourceAsStream("day14.txt")) {
-            Map<Integer, Integer> mem = new HashMap<>();
+            Map<String, Integer> mem = new HashMap<>();
+            toLinesStream(requireNonNull(input))
+                .map(PATTERN::matcher)
+                .filter(Matcher::matches)
+                .forEach(new Consumer<>() {
 
-            toActionStream(requireNonNull(input))
-                .forEach(line -> {
-                    if (line.startsWith("mask")) {
-                        mask = line.substring("mask = ".length()).getBytes();
-                        return;
-                    }
-                    final String[] parts = line.split("] = ");
-                    final Integer index = Integer.valueOf(parts[0].substring("mem[".length()));
-                    final Integer value = Integer.valueOf(parts[1]);
-                    final byte[] binaryValue = toBytes(value);
+                    private byte[] mask = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX".getBytes();
 
-                    int vi = binaryValue.length - 1;
-                    for (int i = mask.length - 1; i >= 0 && vi >= 0; i--) {
-                        if ('1' == mask[i]) {
-                            binaryValue[vi] = '1';
-                        } else if ('0' == mask[i]) {
-                            binaryValue[vi] = '0';
+                    @Override
+                    public void accept(Matcher matcher) {
+                        final String maskAsString = matcher.group("mask");
+                        if (maskAsString != null) {
+                            mask = maskAsString.getBytes();
+                            return;
                         }
-                        vi--;
+
+                        final Integer index = Integer.valueOf(matcher.group("index"));
+                        final Integer value = Integer.valueOf(matcher.group("value"));
+                        final byte[] binaryIndex = toBinaryBytes36(index);
+
+                        for (int i = mask.length - 1; i >= 0; i--) {
+                            if ('0' != mask[i]) {
+                                binaryIndex[i] = mask[i];
+                            }
+                        }
+
+                        List<byte[]> indexes = resolveFloatingIndexes(binaryIndex);
+
+                        String binaryString;
+                        for (byte[] newIndex : indexes) {
+                            binaryString = new String(newIndex);
+                            mem.put(binaryString, value);
+                        }
                     }
-                    final String valueInBits = new String(binaryValue);
-                    final Integer finalValue = Integer.valueOf(valueInBits, 2);
-                    mem.put(index, finalValue);
                 });
 
-            log.info("Answer: {}", mem.values().stream().mapToInt(value -> value).sum());
+            log.info("Answer: {}", mem.values().stream().mapToLong(value -> value).sum());
         }
     }
 
-    private static byte[] toBytes(Integer value) {
+    public static List<byte[]> resolveFloatingIndexes(byte[] source) {
+        final ArrayList<byte[]> indexes = new ArrayList<>();
+        indexes.add(source);
+
+        final int length = source.length;
+        for (int i = 0; i < length; i++) {
+            if (source[i] == 'X') {
+                for (int j = 0, indexesSize = indexes.size(); j < indexesSize; j++) {
+                    byte[] first = indexes.get(j);
+                    final byte[] second = Arrays.copyOf(first, length);
+                    first[i] = '0';
+                    second[i] = '1';
+                    indexes.add(second);
+                }
+            }
+        }
+
+        return indexes;
+    }
+
+    private static byte[] toBinaryBytes36(Integer value) {
         final byte[] binaryValue = Integer.toBinaryString(value).getBytes();
         byte[] all = new byte[36];
         Arrays.fill(all, (byte) '0');
@@ -60,7 +95,7 @@ public class Day14 {
         return all;
     }
 
-    public static Stream<String> toActionStream(InputStream input) {
+    public static Stream<String> toLinesStream(InputStream input) {
         return new Scanner(input)
             .useDelimiter("\n")
             .tokens();
