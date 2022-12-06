0434b4521114df0e471c5cee10cdf8aa85f101e8
Nerijus Eimanavičius <nerijus.eimanavicius@telesoftas.com>
2022-12-06 22:26:47 +0200

    refactor: 2022 Day 3: Rucksack Reorganization extract lambdas to named functions

--- y2022-typescript/src/day03/rucksack-reorganization.test.ts
+++ y2022-typescript/src/day03/rucksack-reorganization.test.ts
@@ -1,4 +1,4 @@
-import { part1, part2 } from './rucksack-reorganization';
+import { part1, part2, toPriority, toTrioGroups } from './rucksack-reorganization';
 import fs from 'fs';
 
 let sample: string;
@@ -23,4 +23,37 @@ test('part 2 sample', () => {
 
 test('part 2', () => {
   expect(part2(input)).toBe(2798);
+});
+
+describe('Priority conversion', () => {
+  it.each([
+    ['a', 1],
+    ['n', 14],
+    ['z', 26],
+    ['A', 27],
+    ['E', 31],
+    ['Z', 52]
+  ])('%p to %p', (letter: string, priority: number) => {
+    expect(toPriority(letter)).toBe(priority);
+  });
+});
+
+describe('Group by 3', () => {
+  test('init group before adding item to group index', () => {
+    expect(toTrioGroups([], 'items', 0)[0]).toBeDefined();
+    expect(toTrioGroups([], 'items', 3)[1]).toBeDefined();
+  });
+
+  test('add item to group index', () => {
+    expect(toTrioGroups([], 'items', 0)).toEqual([['items']]);
+    expect(toTrioGroups([[], ['existing']], 'items', 3)).toEqual([[], ['existing', 'items']]);
+  });
+
+  test('reduces array to groups of 3 items', () => {
+    expect(['a', 'b', 'c'].reduce(toTrioGroups, [] as string[][]))
+      .toEqual([['a', 'b', 'c']]);
+
+    expect(['a', 'b', 'c', 'd', 'e', 'f'].reduce(toTrioGroups, [] as string[][]))
+      .toEqual([['a', 'b', 'c'], ['d', 'e', 'f']]);
+  });
 });
--- y2022-typescript/src/day03/rucksack-reorganization.ts
+++ y2022-typescript/src/day03/rucksack-reorganization.ts
@@ -1,25 +1,38 @@
+export const toHalfPair = (x: string) => [x.slice(0, x.length / 2), x.slice(x.length / 2)];
+
+export const toTrioGroups = (groups: string[][], elf: string, index: number) => {
+  const group = Math.floor(index / 3);
+  groups[group] = [...groups[group] || [], elf];
+  return groups;
+};
+
+export const toPairIntersection = (x: string[]) => x[0].split('')
+  .filter(a => x[1].indexOf(a) != -1);
+
+export const toTrioIntersection = (x: string[]) => x[0].split('')
+  .filter(a => x[1].indexOf(a) != -1)
+  .filter(a => x[2].indexOf(a) != -1);
+
+export const toFirst = (x: string[]) => x[0];
+
+export const toPriority = (x: string) => x.charCodeAt(0) - (x >= 'a' ? 97 - 1 : 65 - 27);
+
+export const toSum = (sum: number, value: number) => sum + value;
+
 export function part1(input: string) {
   return input.split('\n')
-    .map(x => [x.slice(0, x.length / 2), x.slice(x.length / 2)])
-    .map(x => x[0].split('').filter(a => x[1].indexOf(a) != -1)[0])
-    .map(x => x.charCodeAt(0) - (x >= 'a' ? 97 - 1 : 65 - 27))
-    .reduce((sum, value) => sum + value, 0);
+    .map(toHalfPair)
+    .map(toPairIntersection)
+    .map(toFirst)
+    .map(toPriority)
+    .reduce(toSum, 0);
 }
 
 export function part2(input: string) {
   return input.split('\n')
-    .reduce(
-      (groups, elf, index) => {
-        const group = Math.floor(index / 3);
-        groups[group] = [...groups[group] || [], elf];
-        return groups;
-      },
-      [] as string[][]
-    )
-    .map(x => x[0].split('')
-      .filter(a => x[1].indexOf(a) != -1)
-      .filter(a => x[2].indexOf(a) != -1)[0]
-    )
-    .map(x => x.charCodeAt(0) - (x >= 'a' ? 97 - 1 : 65 - 27))
-    .reduce((sum, value) => sum + value, 0);
+    .reduce(toTrioGroups, [] as string[][])
+    .map(toTrioIntersection)
+    .map(toFirst)
+    .map(toPriority)
+    .reduce(toSum, 0);
 }
