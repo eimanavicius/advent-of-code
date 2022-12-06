93468f8b13e4a10f444b4f05aa2aa96021932b8c
Nerijus Eimanavičius <nerijus.eimanavicius@telesoftas.com>
2022-12-06 21:51:02 +0200

    refactor: 2022 Day 3: Rucksack Reorganization merge intersection to single map

--- y2022-typescript/src/day03/rucksack-reorganization.ts
+++ y2022-typescript/src/day03/rucksack-reorganization.ts
@@ -16,11 +16,10 @@ export function part2(input: string) {
       },
       [] as string[][]
     )
-    .map(x => [
-      x[0].split('').filter(a => x[1].indexOf(a) != -1),
-      x[0].split('').filter(a => x[2].indexOf(a) != -1),
-    ])
-    .map(x => x[0].filter(a => x[1].includes(a))[0])
+    .map(x => x[0].split('')
+      .filter(a => x[1].indexOf(a) != -1)
+      .filter(a => x[2].indexOf(a) != -1)[0]
+    )
     .map(x => x.charCodeAt(0) - (x >= 'a' ? 97 - 1 : 65 - 27))
     .reduce((sum, value) => sum + value, 0);
 }
