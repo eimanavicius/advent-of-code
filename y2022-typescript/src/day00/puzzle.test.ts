5a0b438e4f3748454bbdd1bcf74ef1c7f7d6ce43
Nerijus Eimanavičius <nerijus.eimanavicius@telesoftas.com>
2022-12-06 21:21:24 +0200

    chore: add sample input to puzzle solution skeleton

--- y2022-typescript/src/day00/puzzle.test.ts
+++ y2022-typescript/src/day00/puzzle.test.ts
@@ -1,18 +1,26 @@
 import { part1, part2 } from './puzzle';
 import fs from 'fs';
 
+let sample: string;
+let input: string;
 
-describe('puzzle module', () => {
-  let input: string;
-  beforeAll(() => {
-    input = fs.readFileSync('src/day00/input.txt', 'utf-8');
-  });
+beforeAll(() => {
+  sample = fs.readFileSync('src/day00/sample.txt', 'utf-8');
+  input = fs.readFileSync('src/day00/input.txt', 'utf-8');
+});
 
-  test('part 1', () => {
-    expect(part1(input)).toBe('answer 1');
-  });
+test('part 1 sample', () => {
+  expect(part1(sample)).toBe('answer 1');
+});
 
-  test('part 2', () => {
-    expect(part2(input)).toBe('answer 2');
-  });
+test('part 1', () => {
+  expect(part1(input)).toBe('answer 1');
+});
+
+test('part 2 sample', () => {
+  expect(part2(sample)).toBe('answer 2');
+});
+
+test('part 2', () => {
+  expect(part2(input)).toBe('answer 2');
 });
