e64e7b32d21ac4df6a34984aa7139f8f53e286d1
Nerijus Eimanavičius <nerijus.eimanavicius@telesoftas.com>
2022-12-11 15:30:24 +0200

    feat: implement 2022 Day 10: Cathode-Ray Tube Part 2

--- y2022-typescript/src/day10/cathode-ray-tube.ts
+++ y2022-typescript/src/day10/cathode-ray-tube.ts
@@ -26,13 +26,22 @@ class Addx implements Instruction {
 class CPU {
   private cycle = 0;
   private snapshots: number[][] = [];
-  private nextCapture = 20;
+  private readonly emptySprite = '........................................';
+  private sprite: string = '###.....................................';
+  private display: string[] = ['', '', '', '', '', '',];
 
-  constructor(private X: number) {
+  constructor(
+    private X: number,
+    private nextCapture: number
+  ) {
   }
 
   run(instruction: Instruction): CPU {
+    this.sprite = this.makeSprite();
     for (let i = 0; i < instruction.cycles; i++) {
+      let column = this.cycle % 40;
+      let row = Math.floor((this.nextCapture-40) / 40);
+      this.display[row] += this.sprite[column] === '#' ? '#' : '.';
       this.cycle++;
       if (this.cycle === this.nextCapture) {
         this.snapshots.push([this.nextCapture, this.X]);
@@ -43,21 +52,41 @@ class CPU {
     return this;
   }
 
+  private makeSprite(): string {
+    const sprite = this.emptySprite.split('');
+    const start = (this.X-1) % 40;
+
+    sprite[start] = '#';
+    sprite[start + 1] = '#';
+    sprite[start + 2] = '#';
+
+    return sprite.join('');
+  }
+
   sumOfSixSignalStrengths(): number {
     return this.snapshots.slice(0, 6)
       .reduce((sum, capture) => sum + (capture[0] * capture[1]), 0);
   }
+
+  drawDisplay(): string {
+    return this.display.map(x => x).join('\n');
+  }
+}
+
+function parseInstructions(input: string): Instruction[] {
+  return input.split(/\r?\n/)
+    .map(x => x.split(' '))
+    .map((x): Instruction => x[0] === 'addx' ? new Addx(parseInt(x[1])) : new Noop());
 }
 
 export function part1(input: string) {
-  return input.split(/\r?\n/)
-    .map(x => x.split(' '))
-    .map((x): Instruction => x[0] === 'addx' ? new Addx(parseInt(x[1])) : new Noop())
-    .reduce((cpu: CPU, instruction: Instruction) => cpu.run(instruction), new CPU(1))
+  return parseInstructions(input)
+    .reduce((cpu: CPU, instruction: Instruction) => cpu.run(instruction), new CPU(1, 20))
     .sumOfSixSignalStrengths();
 }
 
 export function part2(input: string) {
-  return input.split(/\r?\n/)
-    .reduce((count) => count + 2, 0);
+  return parseInstructions(input)
+    .reduce((cpu: CPU, instruction: Instruction) => cpu.run(instruction), new CPU(1, 40))
+    .drawDisplay();
 }
